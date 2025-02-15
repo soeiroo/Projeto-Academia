39cb816f1b69fcef6edc245b156dda040aecbe17
joabedev <joabjatai@gmail.com>
2025-02-15 10:45:18 -0300

    Adicionei um while

--- main.py
+++ main.py
@@ -10,6 +10,22 @@ def user_registration():
   dados['age'] = input("Idade: ")
   
   users.append(dados)
+
+while True:
+    print("Selecione uma opção:")
+    print("1 - Cadastrar novo usuário")
+    print("2 - Listar usuários")
+    print("3 - Sair")
+    print(" ")
+    option = int(input("Escolha uma opção: "))
+    if option == 1:
+        user_registration()   
+    elif option == 3:
+        break
+    else:
+        print("Opção inválida")
+
+print(users)
   
 with open("users.json", "w", encoding="utf-8") as arquivo:
   json.dump(users, arquivo, indent=4, ensure_ascii=False)
