5ce756b4dea01c7f5d303f56eeee7e151da3d903
Pedro Soeiro <friskylt8@gmail.com>
2025-02-16 17:49:28 -0300

    Otimizando/Estilizando o código com a biblioteca Rich

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import json
 import os
+from rich.console import *
+from rich.theme import Theme
+from rich.table import Table
+
+custom_theme = Theme({"success": "bold green", "error": "bold red"})
+console = Console(theme=custom_theme)
 
 #Função que puxa as informações do {users.json} para a variável users. Usem!
 def users_db():
@@ -18,24 +24,43 @@ def clear():
 def user_registration():
     global users 
     
+    console.log(f"Acessando banco de dados...", style="bold underline")
     users_db()
     
     dados = {}
+    dados['status'] = "Ativo"
     dados['name'] = input("Nome completo: ")
-    dados['age'] = int(input("Idade: "))
+    dados['age'] = input("Idade: ")
     #Criar funcionalidade que torna os ID's em números únicos para cada usuário
     dados['id'] = len(users) + 1
-
-    if ( dados['age'] < 16):
+    
+    temp_dados = dados.copy()
+    if ( int(temp_dados['age']) < 16):
       print("Esse usuário não está apto para se registrar.")
       return
+    
+    dados['id'] = str(temp_dados['id'])
 
     users.append(dados)
     with open("users.json", "w", encoding="utf-8") as arquivo:
         json.dump(users, arquivo, indent=4, ensure_ascii=False)
-    print("Usuário cadastrado com sucesso!")
+    console.print("Usuário cadastrado com sucesso!", style="success")
 
-
+def user_tableDB():
+  users_db()
+  
+  table = Table(title="Informações dos clientes")
+  
+  table.add_column("ID", style="orange3")
+  table.add_column("Name", style="deep_sky_blue1")
+  table.add_column("Idade", style="deep_sky_blue1")
+  table.add_column("Status", style="magenta")
+  
+  for user in users:
+    table.add_row(user['id'], user['name'], user['age'], user['status'])
+  
+  console.print(table)
+  
 def user_edition():
     global users  
     
@@ -44,13 +69,29 @@ def user_edition():
 
     dados = {}
     id = input("Id do usuario: ")
+    console.print("Esse usuário atualmente está:\n[1] Ativo\n[2] Inativo")
+    response = int(input(": "))
+    
+    try:  # Converte para inteiro
+      match response:
+        case 1:
+            dados["status"] = "Ativo"
+        case 2:
+            dados["status"] = "Inativo"
+        case _:
+            print('Entrada inválida, tente novamente.')
+    except ValueError:
+      print("Entrada inválida! Digite um número válido.")
+        
     dados['name'] = input("Novo nome completo: ")
-    dados['age'] = int(input("Nova idade: "))
-    dados['id'] = int(id)
+    dados['age'] = input("Nova idade: ")
+    
+    
+    dados['id'] = id
     positionOfUser = -1
     for index, user in enumerate(users):
       if "id" in user:
-          if user["id"] == int(id):
+          if user["id"] == id:
               positionOfUser = index
               break
 
@@ -68,7 +109,7 @@ def users_remove():
 
   users_db()
     
-  temp_ID = int(input("Insira o ID do usuário que deseja remover: "))
+  temp_ID = input("Insira o ID do usuário que deseja remover: ")
   for user in users:
     if ( user['id'] == temp_ID ):
       print(f"Tem certeza que deseja remover a conta de {user['name']} ?")
@@ -88,28 +129,30 @@ def user_details():
 
   print("Detalhes ()")
 
-def users_list():
-  global users
-  try:
-    with open("users.json", "r", encoding="utf-8") as arquivo:
-      users = json.load(arquivo)
-  except (FileNotFoundError, json.JSONDecodeError):
-    users = []
-  
-  for user in users:
-    print(f"{user['id']} - {user['name']}")
+
+#Foi substituido pelo 
+#def users_list():
+#  global users
+#  try:
+#    with open("users.json", "r", encoding="utf-8") as arquivo:
+#      users = json.load(arquivo)
+#  except (FileNotFoundError, json.JSONDecodeError):
+#    users = []
+#  
+#  for user in users:
+#    print(f"{user['id']} - {user['name']}")
 
 
 while True:
-    print("-------FITGYM-------")
-    print("--------------------")
-    print("1 - Cadastrar novo usuário")
-    print("2 - Listar usuários")
-    print("3 - Editar usuários")
-    print("4 - Remover informações de um usuário")
-    print("5 - Sair")
-    print("6 - Teste")
-    print("--------------------")
+    console.print("-------[bold]FITGYM[/]-------")
+    console.print("--------------------")
+    console.print("1 - Cadastrar novo usuário", style="bold pale_green1")
+    console.print("2 - Listar usuários", style="bold deep_sky_blue1")
+    console.print("3 - Editar usuários", style="bold")
+    console.print("4 - Remover usuários", style="bold red")
+    console.print("5 - Sair", style="bold")
+    console.print("6 - Teste")
+    console.print("--------------------")
     option = int(input("Escolha uma opção: "))
     
     if option == 1:
@@ -117,7 +160,7 @@ while True:
       user_registration()   
     elif option == 2:
       clear()
-      users_list()
+      user_tableDB()
     elif option == 3:
       clear()
       user_edition()
@@ -129,11 +172,11 @@ while True:
       break
     elif option == 6:
       clear()
-      print("Use para testar")
+      print("Teste")
     else:
         print("Opção inválida")
     
-    salvar_usuario = input("Deseja voltar ao menu principal? (Y/N) ").lower()
+    salvar_usuario = console.input("[bold]Deseja voltar ao menu principal?[/] [bold orange3](Y/N)[/] ").lower()
     if salvar_usuario == 'n':
         break
     else:
